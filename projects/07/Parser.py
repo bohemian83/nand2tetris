1f90394ab333571ff02a3cbbdac2a07e195ba694
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2024-02-22 22:38:19 +0100

    removed unused variable

--- projects/07/Parser.py
+++ projects/07/Parser.py
@@ -6,11 +6,10 @@ class Parser:
         self.lines = []
 
     def readFile(self, filename):
-        lines = []
-        with open(filename, "r") as fl:
-            for ln in fl:
-                if ln.strip() and ln[0:2] != "//":
-                    self.lines.append(ln.strip())
+        with open(filename, "r") as file:
+            for line in file:
+                if line.strip() and line[0:2] != "//":
+                    self.lines.append(line.strip())
 
         self.fileIterator = iter(self.lines)
 
