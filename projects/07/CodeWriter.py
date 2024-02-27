b511a37aa4530d46a43015c32d0f4b938693dba3
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2024-02-27 22:43:44 +0100

    pass through args

--- projects/07/CodeWriter.py
+++ projects/07/CodeWriter.py
@@ -2,8 +2,8 @@ class CodeWriter:
     def __init__(self) -> None:
         pass
 
-    def write_arithmetic(self):
-        pass
+    def write_arithmetic(self, args):
+        return args
 
-    def write_pushpop(self):
-        pass
+    def write_pushpop(self, args):
+        return args
