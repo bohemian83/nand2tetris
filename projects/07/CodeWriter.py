ea3b7326955686e539e757067dd8a598bed03d46
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2024-03-03 22:32:32 +0100

    added lcl, arg, this, that code translation

--- projects/07/CodeWriter.py
+++ projects/07/CodeWriter.py
@@ -6,4 +6,45 @@ class CodeWriter:
         return args
 
     def write_pushpop(self, args):
-        return args
+
+        command, segment, index = args[0], args[1], args[2]
+
+        match segment:
+            case "constant":
+                # only push command for constant segment
+                return f"@{index}\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1"
+            case "local":
+                if command == "C_PUSH":
+                    return f"@{index}\nD=A\n@LCL\nD=D+M\n@R13\nM=D\n@R13\nA=M\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1"
+                if command == "C_POP":
+                    return f"@{index}\nD=A\n@LCL\nD=D+M\n@R13\nM=D\n@SP\nM=M-1\n@SP\nA=M\nD=M\n@R13\nA=M\nM=D"
+            case "argument":
+                if command == "C_PUSH":
+                    return f"@{index}\nD=A\n@ARG\nD=D+M\n@R13\nM=D\n@R13\nA=M\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1"
+                if command == "C_POP":
+                    return f"@{index}\nD=A\n@ARG\nD=D+M\n@R13\nM=D\n@SP\nM=M-1\n@SP\nA=M\nD=M\n@R13\nA=M\nM=D"
+            case "this":
+                if command == "C_PUSH":
+                    return f"@{index}\nD=A\n@THIS\nD=D+M\n@R13\nM=D\n@R13\nA=M\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1"
+                if command == "C_POP":
+                    return f"@{index}\nD=A\n@THIS\nD=D+M\n@R13\nM=D\n@SP\nM=M-1\n@SP\nA=M\nD=M\n@R13\nA=M\nM=D"
+            case "that":
+                if command == "C_PUSH":
+                    return f"@{index}\nD=A\n@THAT\nD=D+M\n@R13\nM=D\n@R13\nA=M\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1"
+                if command == "C_POP":
+                    return f"@{index}\nD=A\n@THAT\nD=D+M\n@R13\nM=D\n@SP\nM=M-1\n@SP\nA=M\nD=M\n@R13\nA=M\nM=D"
+            case "static":
+                if command == "C_PUSH":
+                    pass
+                if command == "C_POP":
+                    pass
+            case "temp":
+                if command == "C_PUSH":
+                    pass
+                if command == "C_POP":
+                    pass
+            case "pointer":
+                if command == "C_PUSH":
+                    pass
+                if command == "C_POP":
+                    pass
