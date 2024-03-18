bd8f2df8c859627bb6e94f7b8d0ca79bae6933a8
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2024-03-18 22:03:27 +0100

    converter VM_translator to class

--- projects/07/VM_Translator.py
+++ projects/07/VM_Translator.py
@@ -1,52 +1,61 @@
 from Parser import Parser
 from CodeWriter import CodeWriter
-
-# instantiate classes and readFile iterator in parser class
-# input_file = "./StackArithmetic/StackTest/StackTest.vm"
-input_file = "../08/ProgramFlow/FibonacciSeries/FibonacciSeries.vm"
-# input_file = "../08/ProgramFlow/BasicLoop/BasicLoop.vm"
-file_name = input_file[input_file.rfind("/") + 1 : -3]
-folder_name = input_file[0 : input_file.rfind("/")]
-parser = Parser()
-parser.readFile(input_file)
-codewriter = CodeWriter(file_name)
-
-# read in and format file
-with open(input_file, "r") as input:
-    lines = input.readlines()
-    new_lines = []
-    line_number = 0
-    # remove comments, empty lines and spaces before instructions
-    for line in lines:
-        if line.strip() and line[0:2] != "//":
-            line = line.strip().split(" ")[0]
-            new_lines.append(line.strip())
+import os
 
 
-with open(folder_name + "/" + file_name + ".asm", "w") as output_file:
-    while not parser.endOfFile:
-        line = parser.readNextLine()
-        commandType = parser.commandType(line)
-        if commandType != "C_RETURN":
-            first_arg = parser.first_arg()
-            if commandType == "C_ARITHMETIC":
-                args = (commandType, first_arg)
-                line_to_write = codewriter.write_arithmetic(args)
-                output_file.write(f"//{line}\n{line_to_write}\n")
-            elif commandType in ("C_PUSH", "C_POP", "C_CALL", "C_FUNCTION"):
-                second_arg = parser.second_arg()
-                args = (commandType, first_arg, second_arg)
-                line_to_write = codewriter.write_pushpop(args)
-                output_file.write(f"//{line}\n{line_to_write}\n")
-            elif commandType in ("C_GOTO", "C_IF", "C_LABEL"):
-                args = (commandType, first_arg)
-                line_to_write = codewriter.write_branching(args)
-                output_file.write(f"//{line}\n{line_to_write}\n")
-            elif commandType in ("C_CALL", "C_FUNCTION"):
-                second_arg = parser.second_arg()
-                args = (commandType, first_arg, second_arg)
-                line_to_write = codewriter.write_function_call(args)
-                output_file.write(f"//{line}\n{line_to_write}\n")
-        else:
-            line_to_write = codewriter.write_return()
-            output_file.write(f"//{line}\n{line_to_write}\n")
+class VM_Traslator:
+    def __init__(self, source) -> None:
+        self.source = "../08/ProgramFlow/FibonacciSeries/FibonacciSeries.vm"
+        # instantiate classes and readFile iterator in parser class
+        self.file_name = self.source[self.source.rfind("/") + 1 : -3]
+        self.folder_name = self.source[0 : self.source.rfind("/")]
+        self.parser = Parser()
+        self.codewriter = CodeWriter(self.source)
+
+    def read_file(self):
+        # read in and format file
+        self.parser.readFile(self.source)
+        with open(self.source, "r") as input:
+            lines = input.readlines()
+            new_lines = []
+            # remove comments, empty lines and spaces before instructions
+            for line in lines:
+                if line.strip() and line[0:2] != "//":
+                    line = line.strip().split(" ")[0]
+                    new_lines.append(line.strip())
+
+        return new_lines
+
+    def write_file(self) -> None:
+        with open(self.folder_name + "/" + self.file_name + ".asm", "w") as output_file:
+            while not self.parser.endOfFile:
+                line = self.parser.readNextLine()
+                commandType = self.parser.commandType(line)
+                if commandType != "C_RETURN":
+                    first_arg = self.parser.first_arg()
+                    if commandType == "C_ARITHMETIC":
+                        args = (commandType, first_arg)
+                        line_to_write = self.codewriter.write_arithmetic(args)
+                        output_file.write(f"//{line}\n{line_to_write}\n")
+                    elif commandType in ("C_PUSH", "C_POP", "C_CALL", "C_FUNCTION"):
+                        second_arg = self.parser.second_arg()
+                        args = (commandType, first_arg, second_arg)
+                        line_to_write = self.codewriter.write_pushpop(args)
+                        output_file.write(f"//{line}\n{line_to_write}\n")
+                    elif commandType in ("C_GOTO", "C_IF", "C_LABEL"):
+                        args = (commandType, first_arg)
+                        line_to_write = self.codewriter.write_branching(args)
+                        output_file.write(f"//{line}\n{line_to_write}\n")
+                    elif commandType in ("C_CALL", "C_FUNCTION"):
+                        second_arg = self.parser.second_arg()
+                        args = (commandType, first_arg, second_arg)
+                        line_to_write = self.codewriter.write_function_call(args)
+                        output_file.write(f"//{line}\n{line_to_write}\n")
+                else:
+                    line_to_write = self.codewriter.write_return()
+                    output_file.write(f"//{line}\n{line_to_write}\n")
+
+
+vm_translator = VM_Traslator("source")
+vm_translator.read_file()
+vm_translator.write_file()
